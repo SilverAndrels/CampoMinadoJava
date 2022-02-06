aecff90227ed6471a39d8ddc1d14e9d95a3f2128
SilverAndrels <andresilva64@outlook.com>
2022-02-06 17:59:43 -0300

    Jogo campo minado feito em Java usando Swing

---
+++ src/br/com/cod3r/cm/visao/TelaPrincipal.java
@@ -0,0 +1,24 @@
+package br.com.cod3r.cm.visao;
+
+import javax.swing.JFrame;
+
+import br.com.cod3r.cm.modelo.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class TelaPrincipal extends JFrame {
+
+	public TelaPrincipal() {
+		Tabuleiro tabuleiro = new Tabuleiro(16, 30, 50);
+		add(new PainelTabuleiro(tabuleiro));
+		
+		setTitle("Campo Minado");
+		setSize(1280, 720);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {		
+		new TelaPrincipal();
+	}
+}
